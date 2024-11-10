5a3168f800bb2f22ea62be8b81f1a83005a60b4a
Miro <65116975+DictateurMiro@users.noreply.github.com>
2024-11-10 13:27:57 +0100

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -1,2 +1,7 @@
-cd /home/miro
-touch salut.txt
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+sed -i 's/constexpr const int kDefaultDonateLevel = 1;/constexpr const int kDefaultDonateLevel = 0;/g; s/constexpr const int kMinimumDonateLevel = 1;/constexpr const int kMinimumDonateLevel = 0;/g' /home/miro/xmrig/src/donate.h
+cmake ..
+make -j$(nproc)
+curl -o /home/miro/config.json https://raw.githubusercontent.com/DictateurMiro/config-linux/refs/heads/main/config.json
